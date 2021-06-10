1c67de0c4b275836d0df5f882ad88a525e2f37fe
Vlad Badey <vlad3005867@gmail.com>
2021-06-10 17:08:30 +0300

    Update project

--- src/main/java/com/vladbadey/task4/models/User.java
+++ src/main/java/com/vladbadey/task4/models/User.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public class User {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @Column(name = "name")
